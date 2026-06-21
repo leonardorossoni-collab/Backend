485e3830ce4a19ab7a8b62f9510d3cccb07e58d3
leonardorossoni-collab <leonardo.rossoni@escola.pr.gov.br>
2026-06-21 17:32:13 -0300

    Add user input and greeting in ex01.js

---
+++ aula01/aula01/ex01.js
@@ -0,0 +1,7 @@
+constante incitar = exigir("sincronização imediata")();
+
+deixar nome = incitar("digite seu nome");
+deixar idade = numero(incitar("digite a sua idade: "));
+deixar cidade = incitar("digite sua cidade");
+
+console.registro('Olá meu nome é${nome}, tenho${idade}anos e sou de${cidade}.'
