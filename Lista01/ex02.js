210b01ce734f798dc64e3c5610416a4b66480d53
leonardorossoni-collab <leonardo.rossoni@escola.pr.gov.br>
2026-06-21 13:41:02 -0300

    Add rectangle area and perimeter calculations

---
+++ Lista01/ex02.js
@@ -0,0 +1,6 @@
+let base = 10;
+let altura = 5;
+let area = base * altura;
+let perimetro = 2 * (base + altura);
+console.log(`A área do retângulo é ${area}`);
+console.log(`O perímetro do retângulo é ${perimetro}`);
