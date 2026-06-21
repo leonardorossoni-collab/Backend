f28c23b8ef5ca5ec718b0e63d53f11bb57aada01
leonardorossoni-collab <leonardo.rossoni@escola.pr.gov.br>
2026-06-21 14:48:38 -0300

    Correct logic for generating pattern in ex08.js
    Fix syntax errors in nested loops and conditionals.

---
+++ Lista01/ex08.js
@@ -0,0 +1,11 @@
+ for(let linha = 0; linha < 8; linha++);{
+   let texto = "";
+   for(let coluna = 0; coluna <8; coluna++);{
+     if ((linha + coluna) texto = % 2 == 0);{
+       texto = texto + "#";
+     } else {
+       texto = texto + "";
+     }
+   }
+   console.log(texto);
+ }
