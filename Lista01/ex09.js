4c9739bc2f474391b5f7f36b6b3a2002568ce047
leonardorossoni-collab <leonardo.rossoni@escola.pr.gov.br>
2026-06-21 15:26:51 -0300

    Add min function and minArrow function implementations

---
+++ Lista01/ex09.js
@@ -0,0 +1,25 @@
+function min(a, b) {
+
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+
+}
+
+console.log(min(7, 5));
+console.log(min(10, 8 ));
+
+const minArrow = (a, b) => {
+
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+
+}
+
+console.log(minArrow(8, 5));
+console.log(minArrow(3, 10));
