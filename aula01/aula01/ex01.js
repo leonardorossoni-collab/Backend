1884af38c5158897d17ff80fde1af5c492b22e4d
leonardorossoni-collab <leonardo.rossoni@escola.pr.gov.br>
2026-06-21 17:34:48 -0300

    Delete aula01/aula01 directory

--- aula01/aula01/ex01.js
+++
@@ -1,7 +0,0 @@
-constante incitar = exigir("sincronização imediata")();
-
-deixar nome = incitar("digite seu nome");
-deixar idade = numero(incitar("digite a sua idade: "));
-deixar cidade = incitar("digite sua cidade");
-
-console.registro('Olá meu nome é${nome}, tenho${idade}anos e sou de${cidade}.'
