15191388c5d2d18df60d2b6fa0516ce6d20c0000
leonardorossoni-collab <leonardo.rossoni@escola.pr.gov.br>
2026-08-18 10:38:55 -0300

    Update ex01.js

--- Aula01/ex01.js
+++ Aula01/ex01.js
@@ -1,1 +1,7 @@
+const prompt = require("prompt-sync")();
 
+let nome = prompt("Digite seu nome: ");
+let idade = Number(prompt("Digite sua idade: "));
+let cidade = prompt("Digite sua cidade: ");
+
+console.log(`Olá! Meu nome é ${nome}, tenho ${idade} anos e sou de ${cidade}.`);
